ff384fcdb79646ebad434b129829480550429ba6
RockingCoderRushi <72867633+RockingCoderRushi@users.noreply.github.com>
2020-12-02 20:46:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var a;
+var circles=[];
+
+var base1
+function setup() {
+  createCanvas(800,800);
+  stroke(255)
+  
+ 
+  a=height;
+  circles.push(width/2)
+
+  base1 = new Ground(600,205,225,20)
+
+}
+
+function draw() {
+  camera.zoom=camera.zoom+1
+  background(0);  
+  a=a-1;
+
+   for (i=0;i<circles.length;i++)
+{
+	circle(circles[i], height/2,20)
+}
+if(camera.position.x%width===0)
+{
+	circles.push(camera.position.x+width/2)
+}
+  camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+
+base1.display();
+
+ drawSprites();
+
+
+}
+
+
+
+
+
+
+function keyPressed ()
+{
+  if(keyCode === RIGHT_ARROW)
+  {
+  }
+} 
